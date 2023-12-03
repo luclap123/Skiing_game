b4614ebcb61a4d027dea00a604809d9d002f817f
Lục Lập <boylap7@gmail.com>
2023-12-03 13:54:16 +0700

    trigger effect
    when you touch the finish line or you crash, the effect begin

--- Assets/Script/Detection.cs
+++ Assets/Script/Detection.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 public class Detection : MonoBehaviour
 {
     [SerializeField] float timeReloadScence = 2f;
+    [SerializeField] ParticleSystem particleSystem;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
             // Debug.Log("you lose");
+            particleSystem.Play();
             Invoke("ReloadScence", timeReloadScence);
         }
     }
--- Assets/Script/FinishLine.cs
+++ Assets/Script/FinishLine.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float timeReloadScence = 2f;
+    [SerializeField] ParticleSystem particleSystem;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            particleSystem.Play();
             Invoke("ReloadScence", timeReloadScence);
         }
     }
